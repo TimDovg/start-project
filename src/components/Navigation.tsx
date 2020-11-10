0dcae3cd536aefa40690e2716f4a5dcb629e43e4
TimDovg <timdovg@gmail.com>
2020-11-10 20:27:49 +0200

    main container for planets was created

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Navigation: React.FC = () => {
-    const title = 'Navigation'
+    const history = useHistory()
 
     return (
-        <div>
-            {title}
-        </div>
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <div
+                className="navbar-brand cursor-pointer font-italic"
+                onClick={() => history.push('/')}
+            >
+                Stars Wars
+            </div>
+            <div className="navbar-brand cursor-default m-lg-auto text-light">PLANETS</div>
+        </nav>
     )
 }
 
